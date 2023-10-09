bf95b2bf9ce428b50978c4fcbefae57cbc609c24
nacedodev <nacedo.dev@gmail.com>
2023-10-09 10:11:27 +0200

    basic characters

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 console.log('Script cargado correctamente.')
 
+class Personaje {
+    nombre = null
+    raza = null
+    clase = null
+  
+  hablar(text){
+    let textoMejorado = `<br/> ${this.nombre}: "${text}"`
+    document.getElementById('historia').innerHTML += textoMejorado
+  }
+}
+
+
+let bilbo = new Personaje()
+bilbo.nombre = 'Bilbo Bolsón'
+let gandalf = new Personaje()
+gandalf.nombre = 'Gandalf el Gris'
+console.log(bilbo)
+console.log(gandalf)
+
+gandalf.hablar('Buenos dias mi viejo amigo')
+bilbo.hablar('Gandalf me alegro de verte')
+
 
